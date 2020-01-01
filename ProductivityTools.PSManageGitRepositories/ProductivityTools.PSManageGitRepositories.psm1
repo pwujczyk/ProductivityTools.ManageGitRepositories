437dd163343709a0b81e155da83fe17f76ff8138
pwujczyk <pwujczyk@ecovadis.com>
2020-01-01 19:47:45 +0100

    working and published version

--- ProductivityTools.PSManageGitRepositories/ProductivityTools.PSManageGitRepositories.psm1
+++ ProductivityTools.PSManageGitRepositories/ProductivityTools.PSManageGitRepositories.psm1
@@ -1,6 +1,60 @@
-<#
-	My Function
-#>
-function Get-Function {
-
+function Push-GitRepositories([string] $Directory)
+{
+	$directories=Get-ChildItem $Directory   | ?{ $_.PSIsContainer }
+	foreach($directory in $directories)
+	{
+		cd $directory.FullName
+		if($(Test-Path ".git") -eq $true)
+		{
+			git checkout -b "AutoCommitBranch"
+			git add .
+			git commit -m "Automatic Commit"
+			$remote=git remote
+			git push --all $remote
+		}
+	}	
 }
+
+function Pull-GitRepositories([string] $Directory)
+{
+	$directories=Get-ChildItem $Directory   | ?{ $_.PSIsContainer }
+	#$directories | ForEach-Object - {
+	foreach ($dir in $directories)
+	{
+		$directoryPath=$dir.FullName
+		Write-Host "Checking repository $directoryPath"
+		cd $directoryPath
+		if($(Test-Path ".git") -eq $true)
+		{
+			git pull
+		}
+	}	
+}
+
+function Get-GitRepositoriesStatus([string] $Directory)
+{
+	Push-Location 
+	$directories=Get-ChildItem $directory   | ?{ $_.PSIsContainer }
+	foreach($dir in $directories)
+	{
+		$directoryPath=$dir.FullName
+		cd $directoryPath
+		if($(Test-Path ".git") -eq $true)
+		{
+			$status=git status -u
+			if ($status -contains "nothing to commit, working tree clean")
+			{
+				Write-Host "Git repository $directoryPath - working tree clean" -ForegroundColor Green
+			}
+			else
+			{
+				Write-Host "Git repository $directoryPath - dirty" -ForegroundColor Red
+			}
+		}
+	}	
+	Pop-Location
+}
+
+Export-ModuleMember Push-GitRepositories
+Export-ModuleMember Pull-GitRepositories
+Export-ModuleMember Get-GitRepositoriesStatus
---
+++ ProductivityTools.PSManageGitRepositories/Runner.ps1
@@ -0,0 +1,6 @@
+clear
+
+cd $PSScriptRoot
+Import-Module .\ProductivityTools.PSManageGitRepositories.psm1 -Force
+#Get-GitStatus d:\GitHub
+Pull-GitRepositories d:\GitHub
